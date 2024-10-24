e1e590da9a9719d5db842b0f8d61be8508362a67
Mathieu Lamouroux <mathieu.lamouroux@epitech.eu>
2024-10-24 17:24:17 +0200

    [FIX]: Discord REST API call

--- server/controllers/discord.js
+++ server/controllers/discord.js
@@ -1,23 +1,91 @@
 const dotenv = require('dotenv');
 dotenv.config();
-const { Client, GatewayIntentBits, ChannelType } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+// const { Client, GatewayIntentBits, ChannelType } = require('discord.js');
+// const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const axios = require('axios');
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    const guild = client.guilds.cache.get(process.env.DISCORD_GUILD_ID);
-    createChannel(guild, 'test');
-});
-
-function getChannelIDs(fetch) {
-    return client.guilds.channels.fetch(process.env.DISCORD_GUILD_ID);
+const headers = { 
+    Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
+    ContentType: 'application/x-www-form-urlencoded',
+    Accept: 'application/json',
 };
 
-function createChannel(guild, channelName) {
-    guild.channels.create({
-        name: channelName,
-        type: ChannelType.GuildText,
-    })
+async function fetchTest() {
+    const body = {
+        content: 'Hello, world!',
+    }
+    axios.post(`https://discord.com/api/v10/channels/1298957564062732330/messages`, body, { headers })
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
 }
 
-client.login(process.env.DISCORD_TOKEN);
+async function uploadFile() {
+    const body = {
+        content: "a",
+        attachment: './test.txt'
+    }
+    axios.post(`https://discord.com/api/v10/channels/1298957564062732330/messages`, body, { headers })
+}
+
+function getChannelIDs() {
+    const guild = client.guilds.cache.get(process.env.DISCORD_GUILD_ID);
+    const channels = guild.channels.cache;
+    const channelIDs = { categories: {}, text: {}, voice: {} };
+    channels.forEach(channel => {
+        switch (channel.type) {
+            case ChannelType.GuildCategory:
+                channelIDs.categories[channel.name] = channel.id;
+                break;
+            case ChannelType.GuildText:
+                channelIDs.text[channel.name] = channel.id;
+                break;
+            case ChannelType.GuildVoice:
+                channelIDs.voice[channel.name] = channel.id;
+                break;
+        }
+    });
+    return channelIDs;
+};
+
+async function createCategory(categoryName) {
+    try {
+        // await client.login(process.env.DISCORD_TOKEN);
+        client.on('ready', async () => {
+            const guild = client.guilds.cache.get(process.env.DISCORD_GUILD_ID);
+            guild.channels.create({
+                name: categoryName,
+                type: ChannelType.GuildCategory,
+            })
+            console.log(`Created category ${categoryName}`);
+            // await client.destroy(process.env.DISCORD_TOKEN);
+        });
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function createChannel(channelName, parentCategory) {
+    try {
+        // await client.login(process.env.DISCORD_TOKEN);
+        client.on('ready', async () => {
+            const guild = client.guilds.cache.get(process.env.DISCORD_GUILD_ID);
+            guild.channels.create({
+                name: channelName,
+                type: ChannelType.GuildText,
+                parent: parentCategory,
+            })
+            console.log(`Created channel ${channelName} in category ${parentCategory}`);
+            // await client.destroy(process.env.DISCORD_TOKEN);
+        });
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// getChannelIDs();
+// fetchTest();
+uploadFile();
